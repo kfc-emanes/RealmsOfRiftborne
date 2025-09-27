54db01fb0cdaa675355fe7669c0961eb560585b8
Mysteray01 <rayartmasbate@gmail.com>
2025-09-27 23:39:59 +0800

    Minor modification for menu also added constructors, setters, and getters

--- Characters/Gunner.java
+++ Characters/Gunner.java
@@ -1,30 +1,6 @@
 public class Gunner extends Character {
-
-    private int hp;
-    private int attack;
-    private int mana;
-    private int defense;
-    private int speed;
-    private int level;
-    private int experience;
-    private String name;
-    private String charClass;
-    private String weapon;
-
-    
     public Gunner() {
-
-        this.hp = 4000;
-        this.attack = 450;
-        this.mana = 600;
-        this.defense = 125;
-        this.speed = 100;
-        this.level = 1;
-        this.experience = 0;
-        this.name = "Selene Arclight";
-        this.charClass = "Gunner";
-        this.weapon = "Rusty Dual Pistol";
-
+        super(4000, 450, 600, 125, 100, 1, 0, "Aria Caelith", "Gunner", "Rusty Dual Pistol");
     }
     
 }
--- Characters/Mage.java
+++ Characters/Mage.java
@@ -1,30 +1,6 @@
 public class Mage extends Character {
-
-    private int hp;
-    private int attack;
-    private int mana;
-    private int defense;
-    private int speed;
-    private int level;
-    private int experience;
-    private String name;
-    private String charClass;
-    private String weapon;
-
-
     public Mage() {
-
-        this.hp = 4000;
-        this.attack = 550;
-        this.mana = 1000;
-        this.defense = 180;
-        this.speed = 100;
-        this.level = 1;
-        this.experience = 0;
-        this.name = "Aria Caelith";
-        this.charClass = "Mage";
-        this.weapon = "Wooden Staff";
-        
+        super(4000, 550, 1000, 180, 100, 1, 0, "Selene Arclight", "Mage", "Wooden Staff");
     }
 
 }
